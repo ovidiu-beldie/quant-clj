c1a5f029e2008da93511f28eb36a584632d790e4
Ovidiu Beldie <ovidiu.beldie@gmail.com>
2012-01-15 19:46:07 +0100

    Implemented mid-point policy for trapezoid integral

--- src/quant/math/integrals/trapezoid.clj
+++ src/quant/math/integrals/trapezoid.clj
@@ -14,3 +14,15 @@
 	
 (def default-policy (struct int-policy default-integrate, 2))
 	
+(defn mid-point-integrate [f a b I N]
+	(let [dx (/ (- b a) N)
+	  		val-x (+ a (/ dx 6))
+				D (* 2 dx (/ 1 3))]
+		(loop [sum 0, x val-x, i 0]
+			(if (= i N)
+				(-> (* dx sum)
+					(+ I)
+					(/ 3))
+				(recur (+ sum (f x) (f (+ x D))) (+ x dx) (inc i))))))
+
+
--- test/quant/test/math/integrals/test_trapezoid.clj
+++ test/quant/test/math/integrals/test_trapezoid.clj
@@ -7,3 +7,6 @@
 	(is (= 31/2 (default-integrate #(* 2 %) 2 5 10 2)))
 	(is (= 1915 (default-integrate #(+ 1 %) 50 100 30 3))))
 
+(deftest test-mid-point-integrate
+	(is (= 52/3 (mid-point-integrate #(* 2 %) 2 5 10 2)))
+	(is (= 7630/3 (mid-point-integrate #(+ 1 %) 50 100 30 3))))
