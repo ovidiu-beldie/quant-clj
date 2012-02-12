71f9d3bc0b61675e500ff8735157e71883387191
Ovidiu Beldie <ovidiu.beldie@gmail.com>
2012-02-12 17:56:53 +0100

    Finished implementation of Segment integral

--- src/quant/math/integrals/segment.clj
+++ src/quant/math/integrals/segment.clj
@@ -1,13 +1,24 @@
 (ns quant.math.integrals.segment
 	(:use   
 		[quant.math.integrals.core]
-		[incanter.core :only (abs)]
-		[clojure.stacktrace]))
+		[incanter.core :only (abs)]))
 
 (defrecord Segment [in])
 
-(defn segment [intervals]
-; check intervals
-	(let [arg (make-integrator 1 1
+(extend-type Segment
+	Integrable
 
+	(integrate [{:keys [in]} f a b]
+		(let [dx (/ (- b a) (in :intervals))
+					end (- b (* 0.5 dx))]
+			(loop [x (+ a dx), sum (* 0.5 (+ (f a) (f b)))]
+				(if (>= x end)
+					(* sum dx)
+					(recur (+ x dx) (+ sum (f x))))))))
+
+(defn segment [intervals]
+	(if (pos? intervals)
+		(let [arg (make-integrator 1 1)]
+			(Segment. (merge arg {:intervals intervals})))
+		(throw (IllegalArgumentException. "intervals number must be higher than 0"))))
 
