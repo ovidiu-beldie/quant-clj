f8704a45e537c6ed2ecec33c2e599006dee169dc
Ovidiu Beldie <ovidiu.beldie@gmail.com>
2012-02-29 22:54:20 +0100

    Added functions for matrix creation and setting of the main diagonal.
    Done some minor refactoring.
    
    Signed-off-by: Ovidiu Beldie <ovidiu.beldie@gmail.com>

--- src/quant/math/matrix.clj
+++ src/quant/math/matrix.clj
@@ -1,11 +1,26 @@
 (ns quant.math.matrix)
 
+(defn matrix [rows cols vals]
+  "Creates a matrix with the number of rows
+    and columns equal to the first 2 params 
+    and with values provided by the last param"
+  (partition cols (take (* rows cols) vals)))
+
 (defn column [m j]
   "Return colum j of matrix m"
   (map #(nth % j) m))
 
+(defn assoc-elem [row elem i]
+  "Returns row with elem replacing
+    the element at index i"
+  (assoc row i elem))
+
 (defn assoc-column [m col j]
   "Return the matrix m with column col at index j"
-  (let [assoc-elem (fn [row elem]
-                      (assoc row j elem))]
-    (map assoc-elem m col)))
+    (map assoc-elem m col (repeat j)))
+
+(defn set-main-diag [m vals]
+  "Sets the main diagonal of the matrix m
+    with the values from vals"
+    (map assoc-elem m vals (range)))
+
--- test/quant/test/math/test_matrix.clj
+++ test/quant/test/math/test_matrix.clj
@@ -5,6 +5,12 @@
 
 (def m  [[1 2 3] [4 5 6] [7 8 9]])
 (def m1 [[11 2 3] [41 5 6] [71 8 9]])
+(def m2 [[1 2 3 4 5] [6 7 8 9 10] [11 12 13 14 15]])
+(def m3  [[100 2 3] [4 101 6] [7 8 102]])
+
+(deftest test-matrix
+  (is (= m (matrix 3 3 (range 1 10))))
+  (is (= m2 (matrix 3 5 (range 1 100)))))
 
 (deftest test-column
   (is (= '(1 4 7) (column m 0)))
@@ -13,3 +19,6 @@
 
 (deftest test-assoc-column
   (is (= m1 (assoc-column m [11 41 71] 0))))
+
+(deftest test-set-main-diag
+  (is (= m3 (set-main-diag m (range 100 200)))))
