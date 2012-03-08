f67f7b8d06ebf413ed3e79be368b29429c2af3c1
Ovidiu Beldie <ovidiu.beldie@gmail.com>
2012-03-08 08:15:59 +0100

    Added Colt library to project
    Signed-off-by: Ovidiu Beldie <ovidiu.beldie@gmail.com>

--- project.clj
+++ project.clj
@@ -1,6 +1,8 @@
+
 (defproject quant "1.0.0-SNAPSHOT"
-  :description "FIXME: write description"
+  :description "quant-clj, an implementation of quantLib in Clojure"
   :dependencies [	[org.clojure/clojure "1.3.0"]
 									[org.clojure/clojure-contrib "1.2.0"]	
 									[incanter "1.3.0-SNAPSHOT"]
+                  [colt "1.2.0"]
 								])
