fde969edda3c19ba042119503cc619dc45498634
Ovidiu Beldie <ovidiu.beldie@gmail.com>
2012-03-12 12:30:58 +0100

    Orthogonal projections finished
    Signed-off-by: Ovidiu Beldie <ovidiu.beldie@gmail.com>

--- src/quant/math/matrix_utilities/orthogonal_projections.clj
+++ src/quant/math/matrix_utilities/orthogonal_projections.clj
@@ -10,18 +10,12 @@
 
 (ns quant.math.matrix-utilities.orthogonal-projections
   (:use [quant.math.matrix :only (matrix transpose count-rows count-cols)]
-;        [clojure.contrib.logging :only (spy)]
         [incanter.core :only (sqrt abs)]))
 
 (defstruct orthog-proj-args :orig-vecs :ortho-norm-vecs :valid-vecs :tolerance :multiplier-cutoff :curr-vec)
-(defstruct orthogonal-projections :projected-vecs :valid-vecs)
-
 
 (defn inner-product [m1 r1 m2 r2]
-  (do
-  ;(prn "m1=" m1 "r1=" r1 "m2=" m2 "r2=" r2)
-  ;(prn "inner-product=" (reduce + (map * (m1 r1) (m2 r2))))
-  (reduce + (map * (m1 r1) (m2 r2)))))
+  (reduce + (map * (m1 r1) (m2 r2))))
 
 (defn norm-squared [m r]
   (inner-product m r m r))
@@ -29,47 +23,26 @@
 (defn norm [m r]
   (sqrt (norm-squared m r)))
 
-
 (defn update-ortho-norm-vecs [orig-vecs ortho-norm-vecs valid-vecs k j type]
   (let [pred (fn [l] (and (valid-vecs l) (not (= l j))))
-        _ (prn "update : entered: onv=" ortho-norm-vecs "k=" k "j=" j)
         onv-row (if (= type :type-1) k j)
         range-max (if (= type :type-1) k (inc k))
         dot-prods (for [l (range range-max) :when (pred l)] (inner-product ortho-norm-vecs onv-row ortho-norm-vecs l))
-        _ (prn "update : dot-prods=" dot-prods)
         ordered-onv (map vector ortho-norm-vecs (range range-max))
-        ;_ (prn "update : ordered-onv=" ordered-onv)
         onv-filtered (for [onv ordered-onv :when (pred (second onv))] (first onv))
-        ;_ (prn "update : onv-filtered=" onv-filtered)
         comp-onv-elem (fn [init-onv-elem onv-filtered-col]
-                        (do
-                        ;(prn "update comp-elem: init-onv-elem =" init-onv-elem "onv-filtered-col=" onv-filtered-col "dot-prods=" dot-prods)
-                        ;(prn "comp-onv-elem ret:" (reduce - init-onv-elem (map * dot-prods onv-filtered-col)))
-                        ;(prn "update comp-elem =" (reduce - init-onv-elem (map * dot-prods onv-filtered-col)))
-                        (reduce - init-onv-elem (map * dot-prods onv-filtered-col))))
-        onv-row (if (= type :type-1) k j)
-        ]
+                        (reduce - init-onv-elem (map * dot-prods onv-filtered-col)))
+        onv-row (if (= type :type-1) k j)]
     (if (empty? onv-filtered)
-      (do
-      (prn "update : empty=true, exit=" ortho-norm-vecs)
-      ortho-norm-vecs)
-      (let [_ (prn "update : calling map comp-onv-elem row=" (ortho-norm-vecs onv-row) "onv-filtered-transp=" (transpose onv-filtered))
-            new-row (map comp-onv-elem (ortho-norm-vecs onv-row) (transpose onv-filtered))
-            _ (prn "update : new row=" new-row)
-           ]
+      ortho-norm-vecs
+      (let [new-row (map comp-onv-elem (ortho-norm-vecs onv-row) (transpose onv-filtered))]
       (assoc ortho-norm-vecs onv-row new-row)))))
 
 
 (defn ortho-normal-basis-iter [orig-vecs tolerance j ortho-norm-vecs valid-vecs k]
-  (let [_ (prn "Iteration start--------------------- k=" k "j=" j)
-        _ (prn "iter: iter start onv=" ortho-norm-vecs)
-        onv (assoc ortho-norm-vecs k (orig-vecs k))
-        _ (prn "iter: before if k!=j onv=" onv)
-       ]
+  (let [onv (assoc ortho-norm-vecs k (orig-vecs k))]
     (if (and (not (= k j)) (valid-vecs k))
-      (let [;_ (prn "XXXXXXXXX ortho-normal-basis-iter: k=" k "j="j)
-            upd-onv (update-ortho-norm-vecs orig-vecs onv valid-vecs k j :type-1)
-            _ (prn "iter: after l loop : onv=" upd-onv)
+      (let [upd-onv (update-ortho-norm-vecs orig-vecs onv valid-vecs k j :type-1)
             norm-before-scaling (norm upd-onv k)]
         (if (< norm-before-scaling tolerance)
           {:valid-vecs (assoc valid-vecs k false) :onv upd-onv}
@@ -84,49 +57,31 @@
     (loop [k 0, vecs {:valid-vecs valid-vecs, :onv ortho-norm-vecs}]
       (if (= k num-vecs)
         vecs
-        (do
-        (prn "loop end of iter-1 results=" vecs)
-        (recur (inc k) (onbi (vecs :onv) (vecs :valid-vecs) k)))))))
+        (recur (inc k) (onbi (vecs :onv) (vecs :valid-vecs) k))))))
 
 (defn handle-valid-vec [orig-vecs {:keys [orig-vecs ortho-norm-vecs valid-vecs curr-vec multiplier-cutoff] :as arg} j]
-  (let [_ (prn "hvv : enter - onv=" ortho-norm-vecs)
-        res (ortho-normal-basis-loop arg j)
-        _ (prn "hvv loop results=" res)
+  (let [res (ortho-normal-basis-loop arg j)
         onv (update-ortho-norm-vecs orig-vecs (res :onv) (res :valid-vecs) (dec (count-rows orig-vecs)) j :type-2)
-        _ (prn "hvv after norm onv=" onv)
         proj-on-orig-dir (inner-product orig-vecs j onv j)
         size-muliplier (/ (norm-squared orig-vecs j) proj-on-orig-dir)
-        _ (prn "hvv: norm-squared=" (norm-squared orig-vecs j) "proj-on-orig-dir=" proj-on-orig-dir "size-muliplier=" size-muliplier)
         ]
     (if (< (abs size-muliplier) multiplier-cutoff)
-      (do
-      (prn "hvv: size mult < mul-cutoff, set curr-vec,size-muliplier=" size-muliplier "multiplier-cutoff=" multiplier-cutoff "(onv j)=" (onv j))
-      (assoc arg :ortho-norm-vecs onv, :valid-vecs valid-vecs, :curr-vec (map #(* % size-muliplier) (onv j))))
-      (do
-      (prn "hvv: size mult >= mul-cutoff, NOT set curr-vec, size-muliplier=" size-muliplier "multiplier-cutoff=" multiplier-cutoff "(onv j)=" (onv j))
-      (assoc arg :ortho-norm-vecs onv, :valid-vecs (assoc valid-vecs j false), :curr-vec curr-vec)))))
+      (assoc arg :ortho-norm-vecs onv, :valid-vecs valid-vecs, :curr-vec (map #(* % size-muliplier) (onv j)))
+      (assoc arg :ortho-norm-vecs onv, :valid-vecs (assoc valid-vecs j false), :curr-vec curr-vec))))
 
 (defn orthogonal-projections [orig-vectors multiplier-cutoff tolerance]
   (let [hvc (partial handle-valid-vec orig-vectors)
         dimension (count-cols orig-vectors)
         number-vecs (count-rows orig-vectors)
         ortho-norm-vecs (matrix number-vecs dimension (repeat 0))
-        ;_ (prn "orthogonal-projections ortho-norm-vecs=" ortho-norm-vecs) 
         valid-vecs (vec (repeat number-vecs true))
         curr-vec (repeat dimension 0)
-        ;_ (prn orig-vectors ortho-norm-vecs valid-vecs tolerance multiplier-cutoff curr-vec)
         init-arg (struct orthog-proj-args orig-vectors ortho-norm-vecs valid-vecs tolerance multiplier-cutoff curr-vec)]
   (loop [j 0, arg init-arg, projected-vecs []]
     (if (= j number-vecs)
-      ;(struct orthogonal-projections projected-vecs (arg :valid-vecs))
       {:vectors projected-vecs :valid-vecs (arg :valid-vecs)}
       (let [new-arg (if (valid-vecs j)
-                      (do
-                      ;(prn "calling handle-valid-vec")
-                      (handle-valid-vec orig-vectors arg j))
-                      arg)
-            _ (prn "op: end of iter j=" j "res=" new-arg)
-            _ (prn "MMMMMM curr-vec=" (new-arg :curr-vec))
-           ]
+                      (handle-valid-vec orig-vectors arg j)
+                      arg)]
         (recur (inc j), new-arg, (conj projected-vecs (new-arg :curr-vec))))))))
             
--- test/quant/test/math/matrix_utilities/test_orthogonal_projections.clj
+++ test/quant/test/math/matrix_utilities/test_orthogonal_projections.clj
@@ -19,11 +19,8 @@
             [java.lang Integer]))
 
 (def dim 1000)
-;(def dim 3)
 (def nb-vecs 50)
-;(def nb-vecs 3)
 (def multiplier 100)
-;(def multiplier 10)
 (def tolerance 1e-6)
 (def seed 1)
 (def accept-err 1e-11)
@@ -31,34 +28,22 @@
 (def random-series (repeatedly #(.nextInt rng)))
 (def rs (map #(/ (abs %) Integer/MAX_VALUE) random-series))
 (def test-matrix (matrix nb-vecs dim rs))
-;(def test-matrix (matrix nb-vecs dim (range)))
-;(def test-matrix [[0.9094735984268941 0.5672659280557493 0.06925364586955572] 
-;                  [0.21533483276857754 0.9950156812532878 0.17185625441924496] 
-;                  [0.4932097198875666 0.012768743565664042 0.3805294634683661]])
 (def ordered-test-matrix (map vector test-matrix (range)))
 (def projector (orthogonal-projections test-matrix multiplier tolerance))
 
 (defn test1 []
-  (let [;_ (prn "vectors=" (projector :vectors))
-        pred-rows (fn[i j]
+  (let [pred-rows (fn[i j]
                (and ((projector :valid-vecs) j) (not (= i j))))
         comp-dot-prod (fn [r1 r2]
                         (reduce + (map * r1 r2)))
         pred-prods (fn [p]
-                     (> (abs p) accept-err))  
-        ;_ (prn "OOOO ordered-test-matrix=" ordered-test-matrix)
-        ]
+                     (> (abs p) accept-err))]
     (loop [i 0, nb-errors 0]
       (if (= i nb-vecs)
         nb-errors
         (if ((projector :valid-vecs) i)
           (let [filtered-test-m (for [row ordered-test-matrix :when (pred-rows i (second row))] (first row))
-                _ (prn "OOOOO filtered-test-m=" filtered-test-m "i=" i)
-                _ (prn "OOOOO vectors=" (projector :vectors))
-                ;_ (prn "OOOOO (vector i)=" ((projector :vectors) i))
-                dot-prods (map (partial comp-dot-prod ((projector :vectors) i)) filtered-test-m)
-                _ (prn "dot-prods=" dot-prods)
-               ]
+                dot-prods (map (partial comp-dot-prod ((projector :vectors) i)) filtered-test-m)]
             (recur (inc i) (+ nb-errors (count (filter pred-prods dot-prods)))))
           (recur (inc i) nb-errors))))))
 
@@ -67,22 +52,11 @@
         filtered-test-m (for [row ordered-test-matrix :when (pred-rows (second row))] (first row))
         ordered-vecs (map vector (projector :vectors) (range))
         filtered-vecs (for [row ordered-vecs :when (pred-rows (second row))] (first row))
-        inner-prod (fn [r1 r2]
-                     (do
-                     ;(prn "inner-prod: r1=" r1 "r2=" r2)
-                     ;(prn "inner-prod ret=" (reduce + (map * r1 r2)))
-                     (reduce + (map * r1 r2))))
-        ;_ (prn "test2: filtered-test-m=" filtered-test-m "vectors=" (projector :vectors) "valids=" (projector :valid-vecs))
+        inner-prod (fn [r1 r2] (reduce + (map * r1 r2)))
         in-prod-orig  (map inner-prod filtered-vecs filtered-test-m)
         norm-sq       (map inner-prod filtered-test-m filtered-test-m)
-        pred-epsilon (fn [x y]
-                       (approx= x y accept-err))
-        dif (map pred-epsilon in-prod-orig norm-sq)
-        ;dif (map #(abs (- %1 %2)) in-prod-orig norm-sq)
-        ;_ (prn "in-prod-orig=" in-prod-orig "norm-sq=" norm-sq "dif=" dif)
-        ;pred-pass-test (fn [d] (<= d accept-err))
-        ]
-    ;(count (filter pred-pass-test dif))))
+        pred-epsilon (fn [x y] (approx= x y accept-err))
+        dif (map pred-epsilon in-prod-orig norm-sq)]
     (count (filter false? dif))))
 
 (deftest test-test1
@@ -91,6 +65,3 @@
 (deftest test-test2
   (is (zero? (test2))))
             
-
-
-
