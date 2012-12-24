ad775a226bf262420c9b4103b961827f62aeaab9
Ovidiu Beldie <ovidiu.beldie@gmail.com>
2012-12-24 10:53:31 +0100

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,7 @@
 
 (defproject quant "1.0.0-SNAPSHOT"
   :description "quant-clj, an implementation of quantLib in Clojure"
-  :dependencies [	[org.clojure/clojure "1.3.0"]
-									[org.clojure/clojure-contrib "1.2.0"]	
-									[incanter "1.3.0-SNAPSHOT"]
-                  [colt "1.2.0"]
-								])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+				[org.clojure/algo.generic "0.1.0"]
+				[incanter "1.4.1"]
+				[colt "1.2.0"]])
