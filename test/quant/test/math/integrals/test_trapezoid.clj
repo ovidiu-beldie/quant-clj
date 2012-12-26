5b76083ef4665c0dfefa23db3cfa0033de434f4c
Ovidiu Beldie <ovidiu.beldie@gmail.com>
2012-12-26 23:15:02 +0100

    Removed boilerplate by using macro in integrator test

--- test/quant/test/math/integrals/test_trapezoid.clj
+++ test/quant/test/math/integrals/test_trapezoid.clj
@@ -13,61 +13,53 @@
     [incanter.core :only (sin cos)]
     [incanter.distributions :only (normal-distribution)]
     [quant.math.integrals.core]
+    [quant.common :only (sqr twice)]
     [quant.math.integrals.trapezoid]
     [quant.math.integrals.segment]
     [quant.math.integrals.kronrod]
-    [clojure.contrib.generic.math-functions :only (approx=)]
-    [clojure.test :only (deftest, deftest, is, testing)])) 
+    [clojure.algo.generic.math-functions :only (approx=)]
+    [clojure.test :only (deftest, is, testing)]))
 
-(def tolerance 1.0e-6)
+(def pi Math/PI)
+(def tol 1.0e-6)  ;tolerance
 (def max-iter 10000)
-(defn sqr [x] (* x x))
-(defn const-1 [x] 1)
-(defn id [x] x)
-(def integrate-trapezoid            (partial integrate (trapezoid tolerance max-iter default-policy)))
-(def integrate-simpson              (partial integrate (simpson tolerance max-iter)))
-(def integrate-segment              (partial integrate (segment max-iter)))
-(def integrate-kronrod-adaptive     (partial integrate (kronrod-adaptive tolerance 1000)))
-(def integrate-kronrod-non-adaptive (partial integrate (kronrod-non-adaptive tolerance 100 tolerance)))
+(def in-trapezoid            (partial integrate (trapezoid tol max-iter :default)))
+(def in-simpson              (partial integrate (simpson tol max-iter)))
+(def in-segment              (partial integrate (segment max-iter)))
+(def in-kronrod-adaptive     (partial integrate (kronrod-adaptive tol 1000)))
+(def in-kronrod-non-adaptive (partial integrate (kronrod-non-adaptive tol 100 tol)))
 
-(deftest test-default-integrate
-  (is (= 31/2 (default-integrate #(* 2 %) 2 5 10 2)))
-  (is (= 1915 (default-integrate #(+ 1 %) 50 100 30 3))))
+(deftest test-default-integ
+  (is (= 31/2 (default-integ #(twice %) 2 5 10 2)))
+  (is (= 1915 (default-integ #(inc %) 50 100 30 3))))
 
-(deftest test-mid-point-integrate
-  (is (= 52/3 (mid-point-integrate #(* 2 %) 2 5 10 2)))
-  (is (= 7630/3 (mid-point-integrate #(+ 1 %) 50 100 30 3))))
+(deftest test-midpoint-integ
+  (is (= 52/3 (midpoint-integ #(twice %) 2 5 10 2)))
+  (is (= 7630/3 (midpoint-integ #(inc %) 50 100 30 3))))
+
+(def test-data
+  [[1   (constantly 1) 0 1  "f(x)=1"]
+   [1/2 identity       0 1  "f(x)=x"]
+   [1/3 sqr            0 1  "f(x)=sqr(x)"]
+   [2   sin            0 pi "f(x)=sin(x)"]
+   [0   cos            0 pi "f(x)=cos(x)"]])
+
+(defmacro gen-test [in-fn ex f a b desc]
+  `(is (approx= ~ex (~in-fn ~f ~a ~b) tol) ~desc))
+
+(defn all-tests [in-fn]
+  (doseq [[ex f a b desc :as r] test-data]
+    (gen-test in-fn ex f a b desc)))
 
 (deftest test-integrate
   (testing "Trapezoid integral"
-    (is (approx= 1    (integrate-trapezoid const-1 0 1)   tolerance) "f(x)=1")
-    (is (approx= 1/2  (integrate-trapezoid id 0 1)        tolerance) "f(x)=x")
-    (is (approx= 1/3  (integrate-trapezoid sqr 0 1)       tolerance) "f(x)=sqr(x)")
-    (is (approx= 2    (integrate-trapezoid sin 0 Math/PI) tolerance) "f(x)=sin(x)")
-    (is (approx= 0    (integrate-trapezoid cos 0 Math/PI) tolerance) "f(x)=cos(x)"))
+    (all-tests in-trapezoid))
   (testing "Simpson integral"
-    (is (approx= 1    (integrate-simpson const-1 0 1)   tolerance) "f(x)=1")
-    (is (approx= 1/2  (integrate-simpson id 0 1)        tolerance) "f(x)=x")
-    (is (approx= 1/3  (integrate-simpson sqr 0 1)       tolerance) "f(x)=sqr(x)")
-    (is (approx= 2    (integrate-simpson sin 0 Math/PI) tolerance) "f(x)=sin(x)")
-    (is (approx= 0    (integrate-simpson cos 0 Math/PI) tolerance) "f(x)=cos(x)"))
+    (all-tests in-simpson))
   (testing "Segment integral"
-    (is (approx= 1    (integrate-segment const-1 0 1)   tolerance) "f(x)=1")
-    (is (approx= 1/2  (integrate-segment id 0 1)        tolerance) "f(x)=x")
-    (is (approx= 1/3  (integrate-segment sqr 0 1)       tolerance) "f(x)=sqr(x)")
-    (is (approx= 2    (integrate-segment sin 0 Math/PI) tolerance) "f(x)=sin(x)")
-    (is (approx= 0    (integrate-segment cos 0 Math/PI) tolerance) "f(x)=cos(x)")))
-  (comment
-  (testing "Kronrod adaptive integral"
-    (is (approx= 1    (integrate-kronrod-adaptive const-1 0 1)   tolerance) "f(x)=1")
-    (is (approx= 1/2  (integrate-kronrod-adaptive id 0 1)        tolerance) "f(x)=x")
-    (is (approx= 1/3  (integrate-kronrod-adaptive sqr 0 1)       tolerance) "f(x)=sqr(x)")
-    (is (approx= 2    (integrate-kronrod-adaptive sin 0 Math/PI) tolerance) "f(x)=sin(x)")
-    (is (approx= 0    (integrate-kronrod-adaptive cos 0 Math/PI) tolerance) "f(x)=cos(x)")))
+    (all-tests in-segment)))
+  #_(testing "Kronrod adaptive integral"
+    (all-tests in-kronrod-adaptive))
   (comment
   (testing "Kronrod non adaptive integral"
-    (is (approx= 1    (integrate-kronrod-non-adaptive const-1 0 1)   tolerance) "f(x)=1")
-    (is (approx= 1/2  (integrate-kronrod-non-adaptive id 0 1)        tolerance) "f(x)=x")
-    (is (approx= 1/3  (integrate-kronrod-non-adaptive sqr 0 1)       tolerance) "f(x)=sqr(x)")
-    (is (approx= 2    (integrate-kronrod-non-adaptive sin 0 Math/PI) tolerance) "f(x)=sin(x)")
-    (is (approx= 0    (integrate-kronrod-non-adaptive cos 0 Math/PI) tolerance) "f(x)=cos(x)")))
+    (all-tests in-kronrod-non-adaptive)))
