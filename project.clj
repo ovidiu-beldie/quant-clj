d4fd62d167fbfd230cdf234485907ba5ff0b4ee0
Ovidiu Beldie <ovidiu.beldie@gmail.com>
2012-12-28 19:07:12 +0100

    Minor refactoring (alignement)

--- project.clj
+++ project.clj
@@ -2,8 +2,8 @@
 (defproject quant "1.0.0-SNAPSHOT"
   :description "quant-clj, an implementation of quantLib in Clojure"
   :dependencies [[org.clojure/clojure "1.4.0"]
-				[org.clojure/algo.generic "0.1.0"]
-				[incanter "1.4.1"]
-				[colt "1.2.0"]]
+				 [org.clojure/algo.generic "0.1.0"]
+				 [incanter "1.4.1"]
+				 [colt "1.2.0"]]
   :test-selectors {:current :current
 				   :all (constantly true)})
