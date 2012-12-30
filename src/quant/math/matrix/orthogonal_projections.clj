b543e2a8c8439759ea4b9e3b399dd5ac327db918
Ovidiu Beldie <ovidiu.beldie@gmail.com>
2012-12-30 21:01:49 +0100

    Refactored helper fns in orthogonal projections

--- src/quant/math/matrix/orthogonal_projections.clj
+++ src/quant/math/matrix/orthogonal_projections.clj
@@ -9,18 +9,15 @@
 ; referenced in this library are the propriety of their respective owners
 
 (ns quant.math.matrix.orthogonal-projections
-  (:use [quant.math.matrix.core :only (matrix transpose count-rows count-cols)]
+  (:use [quant.math.matrix.core :only (matrix transpose count-rows count-cols inner-prod)]
         [incanter.core :only (sqrt abs)]))
 
-(declare handle-valid-v update-ortho-norm-v ortho-normal-basis-loop ortho-normal-basis-iter)
+(declare handle-valid-v update-onv onb-loop onb-iter)
 
 (defstruct orthog-proj-args :orig-v :ortho-norm-v :valid-v :tol :m-cutoff :curr-v)
 
-(defn inner-product [m1 r1 m2 r2]
-  (reduce + (map * (m1 r1) (m2 r2))))
-
 (defn norm-squared [m r]
-  (inner-product m r m r))
+  (inner-prod (m r) (m r)))
 
 (defn norm [m r]
   (sqrt (norm-squared m r)))
@@ -44,47 +41,48 @@ Args are: original vecs, multiplier cutoff and tolerance"
         (recur (inc j), new-arg, (conj projected-vecs (new-arg :curr-v))))))))
 
 (defn- handle-valid-v [{:keys [orig-v ortho-norm-v valid-v curr-v m-cutoff] :as arg} j]
-  (let [res (ortho-normal-basis-loop arg j)
-        onv (update-ortho-norm-v orig-v (res :onv) (res :valid-v) (dec (count-rows orig-v)) j :type-2)
-        proj-on-orig-dir (inner-product orig-v j onv j)
-        size-muliplier (/ (norm-squared orig-v j) proj-on-orig-dir)
-        ]
+  (let [res (onb-loop arg j) ;ortho-normal-basis-loop
+        onv (update-onv orig-v (res :onv) (res :valid-v) (dec (count-rows orig-v)) j :type-2)
+        proj-on-orig-dir (inner-prod (orig-v j) (onv j))
+        size-muliplier (/ (norm-squared orig-v j) proj-on-orig-dir)]
     (if (< (abs size-muliplier) m-cutoff)
       (assoc arg :ortho-norm-v onv, :valid-v valid-v, :curr-v (map #(* % size-muliplier) (onv j)))
       (assoc arg :ortho-norm-v onv, :valid-v (assoc valid-v j false), :curr-v curr-v))))
 
-(defn- ortho-normal-basis-loop [{:keys [orig-v valid-v ortho-norm-v tol]} j]
+(defn- onb-loop [{:keys [orig-v valid-v ortho-norm-v tol]} j]
+  "Args are: original vec, valid vec, ortho-normalized vec, tolerance and j"
   (let [num-vecs (count-rows orig-v)
-        onbi (partial ortho-normal-basis-iter orig-v tol j)]
+        onbi (partial onb-iter orig-v tol j)] ;ortho-normal-basis-iter
     (loop [k 0, vecs {:valid-v valid-v, :onv ortho-norm-v}]
       (if (= k num-vecs)
         vecs
         (recur (inc k) (onbi (vecs :onv) (vecs :valid-v) k))))))
 
-(defn- ortho-normal-basis-iter [orig-v tol j ortho-norm-v valid-v k]
-  (let [onv (assoc ortho-norm-v k (orig-v k))]
-    (if (and (not (= k j)) (valid-v k))
-      (let [upd-onv (update-ortho-norm-v orig-v onv valid-v k j :type-1)
-            norm-before-scaling (norm upd-onv k)]
-        (if (< norm-before-scaling tol)
-          {:valid-v (assoc valid-v k false) :onv upd-onv}
-          (let [norm-before-scaling-recip (/ 1 norm-before-scaling)
-                row-k (map #(* % norm-before-scaling-recip) (upd-onv k))]
-            {:valid-v valid-v, :onv (assoc upd-onv k row-k)})))
-      {:valid-v valid-v, :onv onv})))
+(defn- onb-iter [ov tol j onv vv k]
+  "Args are: original vec, tolerance, j, ortho-normalized vec, valid vec, k"
+  (let [onv' (assoc onv k (ov k))]
+    (if (and (not= k j) (vv k))
+      (let [upd-onv (update-onv ov onv' vv k j :type-1)
+            nbs (norm upd-onv k)] ;norm-before-scaling
+        (if (< nbs tol)
+          {:valid-v (assoc vv k false) :onv upd-onv}
+          (let [nbsr (/ 1 nbs) ;norm-before-scaling-recip
+                row-k (map #(* % nbsr) (upd-onv k))]
+            {:valid-v vv, :onv (assoc upd-onv k row-k)})))
+      {:valid-v vv, :onv onv'})))
 
-(defn- update-ortho-norm-v [orig-v ortho-norm-v valid-v k j type]
-  (let [pred (fn [l] (and (valid-v l) (not= l j)))
+(defn- update-onv [ov onv vv k j type]
+  "Args are: original vec, ortho-normalized vec, valid vec, k, j and type"
+  (let [pred (fn [l] (and (vv l) (not= l j)))
         onv-row (if (= type :type-1) k j)
         r-max (range (if (= type :type-1) k (inc k)))
-        dot-prods (for [l r-max :when (pred l)]
-                    (inner-product ortho-norm-v onv-row ortho-norm-v l))
-        ordered-onv (map vector ortho-norm-v r-max)
-        onv-filtered (for [onv ordered-onv :when (pred (second onv))] (first onv))
+        dot-prods (for [l r-max :when (pred l)] (inner-prod (onv onv-row) (onv l)))
+        ord-onv (map vector onv r-max)
+        onv-filtered (for [o ord-onv :when (pred (second o))] (first o))
         comp-onv-elem (fn [init-onv-elem onv-filtered-col]
                         (reduce - init-onv-elem (map * dot-prods onv-filtered-col)))
         onv-row (if (= type :type-1) k j)]
     (if (empty? onv-filtered)
-      ortho-norm-v
-      (let [new-row (map comp-onv-elem (ortho-norm-v onv-row) (transpose onv-filtered))]
-      (assoc ortho-norm-v onv-row new-row)))))
+      onv
+      (let [new-row (map comp-onv-elem (onv onv-row) (transpose onv-filtered))]
+        (assoc onv onv-row new-row)))))
