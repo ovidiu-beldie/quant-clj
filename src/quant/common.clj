3389361e9a40173b1f98cfaa1358e8a15922b576
Ovidiu Beldie <ovidiu.beldie@gmail.com>
2012-12-30 12:20:08 +0100

    Added copyright to src/common.clj

--- src/quant/common.clj
+++ src/quant/common.clj
@@ -1,3 +1,13 @@
+; Copyright (c) 2012 Ovidiu Beldie. All rights reserved.
+; The use and distribution terms for this software are covered by the
+; Eclipse Public License 1.0 (http://opensource.org/licenses/eclipse-1.0.php)
+; which can be found in the file epl-v10.html at the root of this distribution.
+; By using this software in any fashion, you are agreeing to be bound by
+; the terms of this license.
+; You must not remove this notice, or any other, from this software.
+; The initial QuantLib library, Clojure and any other open source software
+; referenced in this library are the propriety of their respective owners
+
 (ns quant.common)
 
 (defn sqr [x] (* x x))
