71b2d7f7d46e41819257097efb029b30c0dd5a10
Ovidiu Beldie <ovidiu.beldie@gmail.com>
2012-12-30 19:53:53 +0100

    Started refactoring orthogonal projections

--- src/quant/math/matrix/orthogonal_projections.clj
+++ src/quant/math/matrix/orthogonal_projections.clj
@@ -12,7 +12,9 @@
   (:use [quant.math.matrix.core :only (matrix transpose count-rows count-cols)]
         [incanter.core :only (sqrt abs)]))
 
-(defstruct orthog-proj-args :orig-vecs :ortho-norm-vecs :valid-vecs :tolerance :multiplier-cutoff :curr-vec)
+(declare handle-valid-v update-ortho-norm-v ortho-normal-basis-loop ortho-normal-basis-iter)
+
+(defstruct orthog-proj-args :orig-v :ortho-norm-v :valid-v :tol :m-cutoff :curr-v)
 
 (defn inner-product [m1 r1 m2 r2]
   (reduce + (map * (m1 r1) (m2 r2))))
@@ -23,65 +25,66 @@
 (defn norm [m r]
   (sqrt (norm-squared m r)))
 
-(defn update-ortho-norm-vecs [orig-vecs ortho-norm-vecs valid-vecs k j type]
-  (let [pred (fn [l] (and (valid-vecs l) (not (= l j))))
+(defn orthogonal-projections [orig-v m-cutoff tol]
+  "Performs the orthogonal projections algo on a collection of vectors.
+Args are: original vecs, multiplier cutoff and tolerance"
+  (let [hvc (partial handle-valid-v orig-v)
+        dim (count-cols orig-v)
+        nr-vecs (count-rows orig-v)
+        ortho-norm-v (matrix nr-vecs dim (repeat 0))
+        valid-v (vec (repeat nr-vecs true))
+        curr-v (repeat dim 0)
+        init-arg (struct orthog-proj-args orig-v ortho-norm-v valid-v tol m-cutoff curr-v)]
+  (loop [j 0, arg init-arg, projected-vecs []]
+    (if (= j nr-vecs)
+      {:vectors projected-vecs :valid-v (arg :valid-v)}
+      (let [new-arg (if (valid-v j)
+                      (handle-valid-v arg j)
+                      arg)]
+        (recur (inc j), new-arg, (conj projected-vecs (new-arg :curr-v))))))))
+
+(defn- handle-valid-v [{:keys [orig-v ortho-norm-v valid-v curr-v m-cutoff] :as arg} j]
+  (let [res (ortho-normal-basis-loop arg j)
+        onv (update-ortho-norm-v orig-v (res :onv) (res :valid-v) (dec (count-rows orig-v)) j :type-2)
+        proj-on-orig-dir (inner-product orig-v j onv j)
+        size-muliplier (/ (norm-squared orig-v j) proj-on-orig-dir)
+        ]
+    (if (< (abs size-muliplier) m-cutoff)
+      (assoc arg :ortho-norm-v onv, :valid-v valid-v, :curr-v (map #(* % size-muliplier) (onv j)))
+      (assoc arg :ortho-norm-v onv, :valid-v (assoc valid-v j false), :curr-v curr-v))))
+
+(defn- ortho-normal-basis-loop [{:keys [orig-v valid-v ortho-norm-v tol]} j]
+  (let [num-vecs (count-rows orig-v)
+        onbi (partial ortho-normal-basis-iter orig-v tol j)]
+    (loop [k 0, vecs {:valid-v valid-v, :onv ortho-norm-v}]
+      (if (= k num-vecs)
+        vecs
+        (recur (inc k) (onbi (vecs :onv) (vecs :valid-v) k))))))
+
+(defn- ortho-normal-basis-iter [orig-v tol j ortho-norm-v valid-v k]
+  (let [onv (assoc ortho-norm-v k (orig-v k))]
+    (if (and (not (= k j)) (valid-v k))
+      (let [upd-onv (update-ortho-norm-v orig-v onv valid-v k j :type-1)
+            norm-before-scaling (norm upd-onv k)]
+        (if (< norm-before-scaling tol)
+          {:valid-v (assoc valid-v k false) :onv upd-onv}
+          (let [norm-before-scaling-recip (/ 1 norm-before-scaling)
+                row-k (map #(* % norm-before-scaling-recip) (upd-onv k))]
+            {:valid-v valid-v, :onv (assoc upd-onv k row-k)})))
+      {:valid-v valid-v, :onv onv})))
+
+(defn- update-ortho-norm-v [orig-v ortho-norm-v valid-v k j type]
+  (let [pred (fn [l] (and (valid-v l) (not= l j)))
         onv-row (if (= type :type-1) k j)
-        range-max (if (= type :type-1) k (inc k))
-        dot-prods (for [l (range range-max) :when (pred l)] (inner-product ortho-norm-vecs onv-row ortho-norm-vecs l))
-        ordered-onv (map vector ortho-norm-vecs (range range-max))
+        r-max (range (if (= type :type-1) k (inc k)))
+        dot-prods (for [l r-max :when (pred l)]
+                    (inner-product ortho-norm-v onv-row ortho-norm-v l))
+        ordered-onv (map vector ortho-norm-v r-max)
         onv-filtered (for [onv ordered-onv :when (pred (second onv))] (first onv))
         comp-onv-elem (fn [init-onv-elem onv-filtered-col]
                         (reduce - init-onv-elem (map * dot-prods onv-filtered-col)))
         onv-row (if (= type :type-1) k j)]
     (if (empty? onv-filtered)
-      ortho-norm-vecs
-      (let [new-row (map comp-onv-elem (ortho-norm-vecs onv-row) (transpose onv-filtered))]
-      (assoc ortho-norm-vecs onv-row new-row)))))
-
-
-(defn ortho-normal-basis-iter [orig-vecs tolerance j ortho-norm-vecs valid-vecs k]
-  (let [onv (assoc ortho-norm-vecs k (orig-vecs k))]
-    (if (and (not (= k j)) (valid-vecs k))
-      (let [upd-onv (update-ortho-norm-vecs orig-vecs onv valid-vecs k j :type-1)
-            norm-before-scaling (norm upd-onv k)]
-        (if (< norm-before-scaling tolerance)
-          {:valid-vecs (assoc valid-vecs k false) :onv upd-onv}
-          (let [norm-before-scaling-recip (/ 1 norm-before-scaling)
-                row-k (map #(* % norm-before-scaling-recip) (upd-onv k))]
-            {:valid-vecs valid-vecs, :onv (assoc upd-onv k row-k)})))
-      {:valid-vecs valid-vecs, :onv onv})))
-      
-(defn ortho-normal-basis-loop [{:keys [orig-vecs valid-vecs ortho-norm-vecs tolerance]} j]
-  (let [num-vecs (count-rows orig-vecs)
-        onbi (partial ortho-normal-basis-iter orig-vecs tolerance j)]
-    (loop [k 0, vecs {:valid-vecs valid-vecs, :onv ortho-norm-vecs}]
-      (if (= k num-vecs)
-        vecs
-        (recur (inc k) (onbi (vecs :onv) (vecs :valid-vecs) k))))))
-
-(defn handle-valid-vec [orig-vecs {:keys [orig-vecs ortho-norm-vecs valid-vecs curr-vec multiplier-cutoff] :as arg} j]
-  (let [res (ortho-normal-basis-loop arg j)
-        onv (update-ortho-norm-vecs orig-vecs (res :onv) (res :valid-vecs) (dec (count-rows orig-vecs)) j :type-2)
-        proj-on-orig-dir (inner-product orig-vecs j onv j)
-        size-muliplier (/ (norm-squared orig-vecs j) proj-on-orig-dir)
-        ]
-    (if (< (abs size-muliplier) multiplier-cutoff)
-      (assoc arg :ortho-norm-vecs onv, :valid-vecs valid-vecs, :curr-vec (map #(* % size-muliplier) (onv j)))
-      (assoc arg :ortho-norm-vecs onv, :valid-vecs (assoc valid-vecs j false), :curr-vec curr-vec))))
-
-(defn orthogonal-projections [orig-vectors multiplier-cutoff tolerance]
-  (let [hvc (partial handle-valid-vec orig-vectors)
-        dimension (count-cols orig-vectors)
-        number-vecs (count-rows orig-vectors)
-        ortho-norm-vecs (matrix number-vecs dimension (repeat 0))
-        valid-vecs (vec (repeat number-vecs true))
-        curr-vec (repeat dimension 0)
-        init-arg (struct orthog-proj-args orig-vectors ortho-norm-vecs valid-vecs tolerance multiplier-cutoff curr-vec)]
-  (loop [j 0, arg init-arg, projected-vecs []]
-    (if (= j number-vecs)
-      {:vectors projected-vecs :valid-vecs (arg :valid-vecs)}
-      (let [new-arg (if (valid-vecs j)
-                      (handle-valid-vec orig-vectors arg j)
-                      arg)]
-        (recur (inc j), new-arg, (conj projected-vecs (new-arg :curr-vec))))))))
-            
+      ortho-norm-v
+      (let [new-row (map comp-onv-elem (ortho-norm-v onv-row) (transpose onv-filtered))]
+      (assoc ortho-norm-v onv-row new-row)))))
